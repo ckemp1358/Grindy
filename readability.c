cd26ae897b9e662f5c3967c379215a7b7dc68232
ckemp1358 <chris.m.kemp.501@gmail.com>
2020-02-12 13:52:05 +0000

    First commit

---
+++ readability.c
@@ -0,0 +1,26 @@
+// include necessary function libraries
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int strln(void);
+int isalpha(void);
+
+int main(void)
+{
+    // ask for a string
+    string s = get_string("Text: ");
+    // create a loop to iterate through the string
+    //increase x with each letter
+    int x;
+    for (int i = 0; i < strln(); i++)
+    {
+        char c;
+        if (isalpha(int c)
+        do (x + 1);
+        else do(x + 0);
+    }
+
+    printf("d%, letters\n", x);
+}
